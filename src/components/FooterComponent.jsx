4ce6ff224de5625be8b8ef05c1ce1dc3621eda65
jesicaahr <jesicaprilia18@gmail.com>
2023-08-15 22:46:10 +0700

    add footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const FooterComponent = () => {
-  return <div>FooterComponent</div>;
+  return (
+    <footer>
+      <Container fluid>
+        <Row className='mt-5'>
+          <Col className='bg-dark text-white py-5 text-center'>
+            Copy &copy; Best Online Shop
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default FooterComponent;
