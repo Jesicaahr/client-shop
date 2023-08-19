5d753dbfb172f426f31eefd648a00dcc1482cd35
jesicaahr <jesicaprilia18@gmail.com>
2023-08-19 23:02:06 +0700

    add image to card

--- src/components/CategoryCardComponent.js
+++ src/components/CategoryCardComponent.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
-const CategoryCardComponent = () => {
+const CategoryCardComponent = ({ category, idx }) => {
+  const images = [
+    '/images/tablets-category.png',
+    '/images/monitors-category.png',
+    '/images/games-category.png',
+    'https://asset.kompas.com/crops/pVMUY7LLi7J6ldUprf977ikGNUs=/0x82:2500x1748/750x500/data/photo/2020/12/10/5fd1bd82c3910.jpg',
+    '/images/tablets-category.png',
+    '/images/monitors-category.png',
+    '/images/games-category.png',
+    'https://asset.kompas.com/crops/pVMUY7LLi7J6ldUprf977ikGNUs=/0x82:2500x1748/750x500/data/photo/2020/12/10/5fd1bd82c3910.jpg',
+  ];
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant='top' src='/images/tablets-category.png' />
+    <Card>
+      <Card.Img
+        crossOrigin='anonymous'
+        variant='top'
+        src={images[idx]}
+        style={{ height: '20rem', objectFir: 'cover' }}
+      />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{category}</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import ProductCarouselComponent from '../components/ProductCarouselComponent';
 import CategoryCardComponent from '../components/CategoryCardComponent';
+import { Row, Container } from 'react-bootstrap';
 
 const HomePage = () => {
+  const categories = [
+    'Tablets',
+    'Monitors',
+    'Games',
+    'Printers',
+    'Softwares',
+    'Cameras',
+    'Books',
+    'Videos',
+  ];
   return (
     <>
       <ProductCarouselComponent />
-
-      <CategoryCardComponent />
+      <Container>
+        <Row xs={1} md={2} className='g-4 mt-5'>
+          {categories.map((category, idx) => (
+            <CategoryCardComponent category={category} idx={idx} key={idx} />
+          ))}
+        </Row>
+      </Container>
     </>
   );
 };
