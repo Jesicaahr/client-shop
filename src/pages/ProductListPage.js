a24c478f1bf963f77a8d77143b9fc651a10370e2
jesicaahr <jesicaprilia18@gmail.com>
2023-08-21 23:55:32 +0700

    layouting product list page

---
+++ src/pages/ProductListPage.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Button, Col, Container, ListGroup, Row } from 'react-bootstrap';
+import SortOptionsComponent from '../components/SortOptionsComponent';
+import ProductForListComponent from '../components/ProductForListComponent';
+import PaginationComponent from '../components/PaginationComponent';
+import PriceFilterComponent from '../components/filterQueryResultOptions/PriceFilterComponent';
+import RatingFilterComponent from '../components/filterQueryResultOptions/RatingFilterComponent';
+import CategoryFilterComponent from '../components/filterQueryResultOptions/CategoryFilterComponent';
+import AttributesFilterComponent from '../components/filterQueryResultOptions/AttributesFilterComponent';
+
+const ProductListPage = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <SortOptionsComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <PriceFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <RatingFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <CategoryFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <AttributesFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button variant='primary'>Primary</Button>
+              <Button variant='danger'>Danger</Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={9}>
+          <ProductForListComponent />
+          <PaginationComponent />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProductListPage;
