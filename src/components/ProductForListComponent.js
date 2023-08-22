cdc546bd8b272eaeea19ee805550304c9f9a318d
jesicaahr <jesicaprilia18@gmail.com>
2023-08-22 23:26:49 +0700

    add star rating

--- src/components/ProductForListComponent.js
+++ src/components/ProductForListComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { RatingView } from 'react-simple-star-rating';
 
 const ProductForListComponent = () => {
   return (
