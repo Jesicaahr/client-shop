b5fc7bb25856b97db17ecbdbedda1180271dc46e
jesicaahr <jesicaprilia18@gmail.com>
2023-08-23 21:42:12 +0700

    improve component for product list

--- src/components/ProductForListComponent.js
+++ src/components/ProductForListComponent.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
-import { RatingView } from 'react-simple-star-rating';
+import { Button, Card, Row, Col } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Rating } from 'react-simple-star-rating';
 
-const ProductForListComponent = () => {
+const ProductForListComponent = ({ images, idx }) => {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant='top' src='/images/tablets-category.png' />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant='primary'>Go somewhere</Button>
-      </Card.Body>
+    <Card style={{ marginTop: '30px', marginBottom: '50px' }}>
+      <Row>
+        <Col lg={5}>
+          <Card.Img variant='top' src={`/images/${images[idx]}-category.png`} />
+        </Col>
+        <Col lg={7}>
+          <Card.Body>
+            <Card.Title>Product Name Lorem Ipsum</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Card.Text>
+              <Rating readonly size={20} initialValue={5} />
+              (2)
+            </Card.Text>
+            <Card.Text className='h4'>
+              $124{' '}
+              <LinkContainer to='/product-details'>
+                <Button variant='danger'>See Product</Button>
+              </LinkContainer>
+            </Card.Text>
+          </Card.Body>
+        </Col>
+      </Row>
     </Card>
   );
 };
